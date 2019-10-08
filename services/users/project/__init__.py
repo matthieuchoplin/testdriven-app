b7901c9fc9db161339b2adb24c8bb9fa70f7cc24
Choplin <matthieu.choplin@made.com>
2019-10-08 20:51:22 +0100

    init

---
+++ services/users/project/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+        'status': 'success',
+        'message': 'pong!'
+    }
+
+
+api.add_resource(UsersPing, '/users/ping')
+
