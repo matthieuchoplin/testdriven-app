0c5cc668f7c9be4d753e4365abf192d317f920a0
Choplin <matthieu.choplin@made.com>
2020-06-19 17:59:12 +0100

    debugtoolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,10 +2,12 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_debugtoolbar import DebugToolbarExtension
 
 # instantiate the db
 db = SQLAlchemy()
+# instantiate the debug toolbar
+toolbar = DebugToolbarExtension()
 
 """
 Here we are using the Application Factory pattern.
@@ -24,6 +26,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
